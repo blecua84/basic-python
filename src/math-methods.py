e103b5da0ed2528865ace7ba36bfe1445b474a21
Jose Javier Blecua <josejavier.blecua@iproov.com>
2022-07-23 12:01:57 +0100

    Add math methods lesson.

---
+++ src/math-methods.py
@@ -0,0 +1,17 @@
+import math
+
+pi = 3.14
+
+x = 1
+y = 2
+z = 3
+
+print("Round: " + str(round(pi)))
+print("Round up: " + str(math.ceil(pi)))
+print("Round down: " + str(math.floor(pi)))
+print("Abs value: " + str(abs(pi)))
+print("Pow number by 2: " + str(pow(pi, 2)))
+print("Sqrt: " + str(math.sqrt(pi)))
+print("Sqrt: " + str(math.sqrt(pi)))
+print("Max value: " + str(max(x, y, z)))
+print("Min value: " + str(min(x, y, z)))
