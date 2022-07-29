5d121a57502e5be1a8df8074ef8c51757bbbcdff
Jose Javier Blecua <josejavier.blecua@iproov.com>
2022-07-29 17:00:40 +0100

    Add basic for loops lesson

---
+++ src/for-loops.py
@@ -0,0 +1,21 @@
+# for loop = a statement that will execute its block of code a limited amount of times
+#
+#     while loop = unlimited
+#     for loop = limited
+import time
+
+# for index in range(10):
+#     print(index)
+
+# for index in range(50, 100+1, 2):
+#     print(index)
+
+# for index in "Jose Javier":
+#     print(index)
+
+# Countdown program
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)
+
+print("Happy New Year!")
