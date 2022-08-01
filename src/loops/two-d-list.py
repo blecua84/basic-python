72bc23962bd14849f3dfa80633201fcc4d5ff7a5
Jose Javier Blecua <josejavier.blecua@iproov.com>
2022-08-01 10:28:43 +0100

    Add 2D list lesson.

---
+++ src/loops/two-d-list.py
@@ -0,0 +1,11 @@
+# 2D lists = a list of lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hot dog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+print(food)
+print(food[0])
+print(food[0][0])
